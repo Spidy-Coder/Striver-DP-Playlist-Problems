9c062cf4395c7ce1f137e64b66ecf1aac96ae522
Paridhi Jain <18ec.paridhijain@gmail.com>
2022-06-20 18:22:25 +0530

    Create Target Sum.cpp

---
+++ Target Sum.cpp
@@ -0,0 +1,34 @@
+
+int findWays(vector<int> &num,int tar) {
+    
+    int n = num.size();
+    vector<vector<int>>dp(n,vector<int>(tar+1, 0));
+    if(num[0] == 0) dp[0][0] = 2;
+    else dp[0][0] = 1;
+    //num[0] = 0
+    if(num[0] != 0 && num[0]<=tar) dp[0][num[0]] = 1;
+    for(int ind=1;ind<n;ind++){
+        for(int sum = 0; sum<=tar;sum++){
+            int notTake = dp[ind-1][sum];
+            int take = 0;
+            if(num[ind] <= sum) take = dp[ind-1][sum-num[ind]];
+            dp[ind][sum] = (notTake + take);
+        }
+    }
+    
+    return dp[n-1][tar];
+                    
+    
+}
+
+int countPartitions(int n, int d, vector<int> &arr) {
+    int totSum = 0;
+    for(auto &it:arr) totSum += it;
+    if(totSum - d < 0 || (totSum-d)%2) return false;
+    return findWays(arr, (totSum-d)/2);
+    
+}
+
+int targetSum(int n, int target, vector<int>& arr) {
+    return countPartitions(n,target,arr);
+}
