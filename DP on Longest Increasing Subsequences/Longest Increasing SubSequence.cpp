6fe70d9d1a431a46b921a71fb61373553c17f5a4
Paridhi Jain <18ec.paridhijain@gmail.com>
2022-07-08 18:56:47 +0530

    Update Longest Increasing SubSequence.cpp

--- DP on Longest Increasing Subsequences/Longest Increasing SubSequence.cpp
+++ DP on Longest Increasing Subsequences/Longest Increasing SubSequence.cpp
@@ -71,5 +71,26 @@ int longestIncreasingSubsequence(int arr[], int n)
     return temp.size();
 }
 
+//------------------------------------------------------
+
+//APPROACH:-5:- ALGORITHM
+
+#include<bits/stdc++.h>
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    vector<int> dp(n,1);
+    int maxi = 1;
+    for(int i=0;i<n;i++){
+        for(int prev=0;prev<i;prev++){
+            if(arr[prev] < arr[i]){
+                dp[i] = max(dp[i], 1+dp[prev]);
+            }
+        }
+        maxi = max(maxi, dp[i]);
+    }
+    return maxi;
+}
+
+
 
 
